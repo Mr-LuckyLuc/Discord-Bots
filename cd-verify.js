8ec30b8a9fe215dc1adc678a4dc3f85f6caf2b67
Lucas <lucas.belmans@student.kdg.be>
2026-05-10 14:01:41 +0200

    codedojo bot

---
+++ cd-verify.js
@@ -0,0 +1,53 @@
+const { Client, GatewayIntentBits, Events, IntentsBitField, SlashCommandBuilder, MessageFlags, REST, Routes } = require('discord.js');
+const client = new Client({ intents: [IntentsBitField.Flags.Guilds, IntentsBitField.Flags.GuildMembers], autoReconnect: true });
+require("dotenv").config({ path: __dirname+'/.env' }); //to start process from .env file
+
+const actualCode = "codercool";
+const roleId = "1502995067793833995";
+
+const command =  {
+	data: new SlashCommandBuilder()
+		.setName('verify')
+		.setDescription('Verify to get the coach role.')
+		.addStringOption((option) => option
+			.setName('code')
+			.setDescription('The code to become a coach (same as the eventbrite code).')
+            .setRequired(true)
+		),
+        
+	async execute(interaction) {
+
+		const sentCode = interaction.options.getString('code');
+        const role = interaction.guild.roles.cache.get(roleId);
+        if (sentCode == actualCode) {
+            interaction.member.roles.add(role);
+            interaction.reply({ content: "You are now a coach", flags: MessageFlags.Ephemeral });
+            return;
+        }
+        interaction.reply({ content: "Not a valid code", flags: MessageFlags.Ephemeral });
+
+	}
+}
+
+const rest = new REST().setToken(process.env.TOKEN);
+
+console.log("Starting")
+
+client.login(process.env.TOKEN).then(async () => {
+    
+    const data = await rest.put(
+        Routes.applicationGuildCommands(process.env.CLIENTID, process.env.GUILDID), //for individual guild
+            { body: [command.data.toJSON()] },
+        );
+});
+
+client.on('ready', () => {
+    console.log("Ready")
+});
+
+client.on(Events.InteractionCreate, (interaction) => {
+    
+    if (interaction.isChatInputCommand() && interaction.commandName == "verify") {
+        command.execute(interaction);
+    }
+});
